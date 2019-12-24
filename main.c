4e47a4d4aad2e060b8412e22e29cdd08c8a962d2
zhengxuntai <302780150@qq.com>
2019-12-24 14:37:08 +0800

    Signed-off-by: zhengxuntai <302780150@qq.com>

--- main.c
+++ main.c
@@ -1,82 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <string.h>
 
 #define  NORMAL_NUM         6
 #define  SPE_NUM            1
-#define  COMBINE_NUM        (SPE_NUM+NORMAL_NUM)
 
-#define  NUM_MAX            49
+#define  COMBINE_NUM                (SPE_NUM+NORMAL_NUM)
+
+#define  NORMAL_NUM_MAX             49
+#define  SPECIAL_NUM_MAX            49
 
 
 #define ANALYSIS_PHASE      30
 #define WEIGHT_SUM      3
 
+uint8_t norNumDb[1024][NORMAL_NUM];
+uint8_t specNumDb[1024][SPE_NUM];
 
-uint8_t *srcDbData;
 
-uint16_t CheckNum(uint8_t *srcData, uint8_t *dstData,  uint8_t normalCnt, uint8_t speCnt)
+// 检测有多少个数值符合
+uint16_t CheckNum(uint8_t *norSrcData, uint8_t *norDstData, uint8_t *speSrcData, uint8_t *speDstData)
 {
     uint8_t i, j, Cnt = 0;
 
-    for(i = 0; i < normalCnt; i++)
+
+    for(i = 0; i < NORMAL_NUM; i++)
     {
-        for(j = 0; j < normalCnt; j++)
-            if(srcData[i] == dstData[j])
+        for(j = 0; j < NORMAL_NUM; j++)
+            if(norSrcData[i] == norDstData[j])
                 Cnt++;
     }
 
-    for(i = 0; i < speCnt; i++)
+    for(i = 0; i < SPE_NUM; i++)
     {
-        for(j = 0; j < speCnt; j++)
-            if(srcData[i+normalCnt] == dstData[normalCnt+j])
+        for(j = 0; j < SPE_NUM; j++)
+            if(speSrcData[j] == speDstData[j])
                 Cnt++;
     }
     return Cnt;
 }
 
+// 检查指定数量有几个符合目标
+uint16_t CheckTheAimNormalNum(uint8_t *srcData, uint8_t *dstData, uint8_t CheckNum)
+{
+    uint16_t i, j, luckyCnt = 0;
+    for(i = 0; i < NORMAL_NUM; i++)
+    {
+        for(j = 0; j < CheckNum; j++)
+        {
+            if(srcData[j] == dstData[i])
+            {
+                luckyCnt ++;
+                break;
+            }
+        }
+    }
+    return luckyCnt;
+}
+
+// 获取一段区间内的统计数据
 void GetDataSum(uint32_t startPhase, uint32_t endPhase, uint8_t *normalSum, uint8_t* speSum)
 {
     uint32_t i, j;
 
-    memset(normalSum, 0, sizeof(normalSum));
-    memset(speSum, 0, sizeof(speSum));
+    memset(normalSum, 0, NORMAL_NUM_MAX);
+    memset(speSum, 0, SPECIAL_NUM_MAX);
 
     for(i = startPhase; i <= endPhase; i++)
     {
-        for(j = 0; j < (NORMAL_NUM+SPE_NUM); j++)
+        for(j = 0; j < (NORMAL_NUM); j++)
         {
-            if(j < NORMAL_NUM)
-                normalSum[srcDbData[COMBINE_NUM*i+j]]++;
-            else
-                speSum[srcDbData[COMBINE_NUM*i+j]]++;
+            normalSum[norNumDb[i][j]-1]++;
+            speSum[specNumDb[i][j]-1]++;
         }
     }
 
-    return analysisData;
+    return;
 }
 
 
 uint8_t* GetWeight(uint8_t * sumData)
 {
     uint16_t i, j;
-    uint8_t weightSum[NUM_MAX];
-    memset(weightSum, 0, NUM_MAX);
-    for(i = WEIGHT_SUM/2; i < NUM_MAX-(WEIGHT_SUM/2))
+    static uint8_t weightSum[NORMAL_NUM_MAX];
+    memset(weightSum, 0, NORMAL_NUM_MAX);
+    for(i = WEIGHT_SUM/2; i < NORMAL_NUM_MAX-(WEIGHT_SUM/2); i++)
     {
-        for(j = i-(WEIGHT_SUM/2); i < WEIGHT_SUM; i++)
+        for(j = i-(WEIGHT_SUM/2); j < WEIGHT_SUM; j++)
         {
             weightSum[i] += sumData[j];
         }
     }
+    return weightSum;
 }
 
 void AnalysisWeight(uint16_t analysisPhaseTimes)
 {
-    uint8_t normalTemp[NUM_MAX], specTemp[NUM_MAX];
+    uint8_t normalTemp[NORMAL_NUM_MAX], specTemp[NORMAL_NUM_MAX];
     GetDataSum(0, ANALYSIS_PHASE, normalTemp, specTemp);
-    for()
 }
 
 
@@ -85,14 +107,13 @@ void AnalysisWeight(uint16_t analysisPhaseTimes)
 
 int main()
 {
-    uint32_t maxPhase;
-    uint8_t *srcDate, *dstData;
+    uint32_t maxPhase, i;
 
     printf("**************Congratulations, make a big asset!**************\n");
 
     for(i = ANALYSIS_PHASE-1; i < maxPhase; i++)
     {
-        srcDate = 
+
     }
 
     return 0;
